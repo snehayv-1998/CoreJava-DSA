132bda17e1d0548f55b8a6f5e46fdb0748359bbe
sneha <sneha.yv@robosoftin.com>
2023-08-27 12:37:43 +0530

    Strings

--- Java Basics Praticals/src/main/java/EString.java
+++ Java Basics Praticals/src/main/java/EString.java
@@ -1,2 +1,29 @@
-package PACKAGE_NAME;public class EString {
+public class EString {
+    public static void main(String[] args) {
+        String a = "Hello"; //a(reference) will be created in stack memory and "Hello" (object) is created in String pool(a place within heap memory)
+        String b = "Hello"; //b(reference) will be created in stack memory and it will point to same "Hello" (object) in String pool
+        System.out.println(a==b); //true because both a and b are pointing to same object in string pool
+
+        String c = new String("World"); //c(reference) will be in stack memory and "Hello" (object) is created outside String pool,in heap memory
+        String d = new String("World"); //d(reference) will be in stack memory and "Hello" (object) is created outside String pool,in heap memory
+        System.out.println(c == d); //false because both c and d are pointing to different object in heap
+        System.out.println(c.equals(d));//true because both c and d has same value
+
+        String e = new String("Hello").intern();//e(reference) will be created in stack memory and it will point to same "Hello" (object) in String pool
+        System.out.println(e==a);//true
+
+       /* String em = null;
+        System.out.println(em.trim()); //error
+      */
+
+        String isdWithSpecialChar = "+1-245";
+        String mobileNumberWithSpecialChar = "+1-2456990085411245";
+        isdWithSpecialChar = isdWithSpecialChar.replaceAll("\\D","");
+        mobileNumberWithSpecialChar = mobileNumberWithSpecialChar.replaceAll("\\D","");
+        System.out.println("isdWithSpecialChar : "+isdWithSpecialChar);
+        System.out.println("mobileNumberWithSpecialChar : "+mobileNumberWithSpecialChar);
+        System.out.println("mobileNumberWithSpecialChar excluding isd : "+(mobileNumberWithSpecialChar.startsWith(isdWithSpecialChar)?mobileNumberWithSpecialChar.substring(isdWithSpecialChar.length()):mobileNumberWithSpecialChar));
+        String str = "A man, a plan, a canal: Panama";
+        System.out.println("A man, a plan, a canal: Panama : "+ str.replaceAll("\\W",""));
+    }
 }
--- Java Basics Praticals/src/main/java/QString.java
+++ Java Basics Praticals/src/main/java/QString.java
@@ -1,2 +1,88 @@
-package PACKAGE_NAME;public class QString {
+import java.util.Arrays;
+
+public class QString {
+    /*
+    Uppercase Letters − A - Z having ASCII values from 65 - 90 where 65 and 90 are inclusive.
+    Lowercase Letter − a - z having ASCII values from 97 - 122 where 97 and 122 are inclusive.
+    Numeric values − 0 - 9 having ASCII values from 48 - 57 where 48 and 57 are inclusive.
+     */
+
+
+
+    public static void main(String[] args) {
+        {
+            /*
+        A phrase is a palindrome if, after converting all uppercase letters into
+        lowercase letters and removing all non-alphanumeric characters, it reads the same forward and backward.
+        Alphanumeric characters include letters and numbers.
+        Given a string s, return true if it is a palindrome, or false otherwise.
+        Example 1:
+        Input: s = "A man, a plan, a canal: Panama"
+        Output: true
+        Explanation: "amanaplanacanalpanama" is a palindrome.
+         */
+            String str = "A man, a plan, a canal: _Panama";
+
+            System.out.println("\n\nBefore converting : "+str);
+
+            StringBuilder stringBuilder = new StringBuilder();
+            for (int i= 0 ; i<str.length();i++){
+                if(isAlphaNumeric(str.charAt(i)))
+                    stringBuilder.append(toLowerCase(str.charAt(i)));
+            }
+
+            System.out.println("\n\nIs palindrome :" + palindrome(stringBuilder));
+        }
+
+        {
+            /*
+            You are given a string s and an integer array indices of the same length.
+            The string s will be shuffled such that the character at the ith position
+            moves to indices[i] in the shuffled string.Return the shuffled string.
+            Example 1:
+            Input: s = "codeleet", indices = [4,5,6,7,0,2,1,3]
+            Output: "leetcode"
+            Explanation: As shown, "codeleet" becomes "leetcode" after shuffling.
+             */
+            String s = "codeleet";
+            int[] indices = {4,5,6,7,0,2,1,3};
+            char c[] = new char[s.length()];
+            for (int i= 0 ; i<s.length();i++){
+                c[indices[i]]= s.charAt(i);// ith position moves to indices[i]
+                /*
+                wrong ans:
+                c[i]= s.charAt(indices[i]); this moves index[i] to i
+                 */
+            }
+            System.out.println("Before shuffling : "+s);
+            System.out.println("After shuffling: "+ String.valueOf(c));
+        }
+    }
+
+    public static char toLowerCase(char c) {
+        if(c >= 'A' && c <= 'Z')  return (char) (c + 32);
+        return c;
+    }
+
+    public static boolean isAlphaNumeric(char c) {
+        return  (c >= '0' && c <= '9') || (c >= 'a' && c <= 'z')  || (c >= 'A' && c <= 'Z')? true : false;
+    }
+
+    public static boolean isUpper(char c) {
+        return (c >= 'A' && c <= 'Z') ? true : false;
+    }
+    public  boolean isLower(char c) {
+        return (c >= 'a' && c <= 'z') ? true : false;
+    }
+
+    private static boolean palindrome(StringBuilder s) {
+        int i = 0;
+        int length =  s.length();
+        while (i < length/2){
+            if(s.charAt(i) != s.charAt(length-1-i)){
+                return false;
+            }
+            i +=1;
+        }return true;
+    }
 }
