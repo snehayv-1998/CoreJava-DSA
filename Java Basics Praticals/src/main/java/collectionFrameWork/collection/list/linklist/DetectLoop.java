b691794754c088637424eed9517fbf297c85e9a0
Sneha Y V <snehayvudupi@gmail.com>
2023-12-25 07:48:43 +0530

    detecting the loop in linked list

--- Java Basics Praticals/src/main/java/collectionFrameWork/collection/list/linklist/DetectLoop.java
+++ Java Basics Praticals/src/main/java/collectionFrameWork/collection/list/linklist/DetectLoop.java
@@ -6,8 +6,20 @@ public class DetectLoop {
         singleLinkedList.addFirstElement(5);
         singleLinkedList.addLastElement(6);
         singleLinkedList.addElement(9,1);
-        singleLinkedList.display();
+        createCycle(singleLinkedList.getHead(),1,2);
         System.out.println("Is cycle present in the linked list: "+cycleDetect(singleLinkedList.getHead()));
+    }
+    static void createCycle(SingleLinkedList.Node head,int node1Index,int node2Index){
+        int cnt1=0;int cnt2=0;
+        SingleLinkedList.Node p1=head;
+        SingleLinkedList.Node p2=head;
+        while(cnt1 != node1Index || cnt2 != node2Index) {
+            if(cnt1 != node1Index)
+            {p1 = p1.next; ++cnt1;}
+            if(cnt2 != node2Index)
+            {p2 = p2.next; ++cnt2;}
+        }
+        p2.next = p1;
     }
     static boolean cycleDetect(SingleLinkedList.Node head) {
         SingleLinkedList.Node slow=head;SingleLinkedList.Node fast=head;
